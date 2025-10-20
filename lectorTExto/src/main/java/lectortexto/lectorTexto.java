c6b87c1757118f03439e2e6af82871e399f767d5
PabloBg19 <pbelascoainguerra@gmail.com>
2025-10-20 08:48:10 +0200

    Update lectorTexto.java

--- lectorTExto/src/main/java/lectortexto/lectorTexto.java
+++ lectorTExto/src/main/java/lectortexto/lectorTexto.java
@@ -8,14 +8,14 @@ public class lectorTexto {
             // Usar getResourceAsStream para leer desde resources
             InputStream is = lectorTexto.class.getClassLoader().getResourceAsStream("archivo.txt");
 
-            if (is == null) {
+            if (is == null) {  //if por si no encuentra
                 throw new FileNotFoundException("No se encontró archivo.txt en resources");
             }
 
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
             String linea;
 
-            while ((linea = br.readLine()) != null) {
+            while ((linea = br.readLine()) != null) {  //bucle para leer linea a linea
                 System.out.println(linea);
             }
             br.close();
@@ -25,3 +25,4 @@ public class lectorTexto {
         }
     }
 }
+
