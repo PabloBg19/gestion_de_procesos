535587e65def8b99acc6c12d8a7d27466d5d61e1
PabloBg19 <pbelascoainguerra@gmail.com>
2025-10-20 08:49:53 +0200

    Update contadorPalabras.java

--- contadorPalabras/src/main/java/contadorpalabras/contadorPalabras.java
+++ contadorPalabras/src/main/java/contadorpalabras/contadorPalabras.java
@@ -5,14 +5,14 @@ import java.io.InputStreamReader;
 
 public class contadorPalabras {
     public static void main(String[] args) {
-        InputStreamReader isr = new InputStreamReader(System.in);
+        InputStreamReader isr = new InputStreamReader(System.in);  //inicializamos input y buffered
         BufferedReader br = new BufferedReader(isr);
         String linea = null;
-        int contadorPalabras = 0;
+        int contadorPalabras = 0;  //inicializamos en 0 para ir leyendo
         try {
-            while ((linea = br.readLine()) != null) {
+            while ((linea = br.readLine()) != null) {  //vamos leyendo linea a linea
                 linea = linea.trim();
-                if (!linea.isEmpty()) {
+                if (!linea.isEmpty()) { //si existe se añade al contador de palabras
                     String[] palabras = linea.split("\\s+");
                     contadorPalabras += palabras.length;
                 }
@@ -29,3 +29,4 @@ public class contadorPalabras {
         }
     }
 }
+
